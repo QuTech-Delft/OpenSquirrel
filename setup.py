590c13e696216b37ac668285962835000c8cb132
pablolh <p.lehenaff@tudelft.nl>
2023-11-06 17:04:24 +0100

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "opensquirrel",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Pablo Le Henaff",
     author_email = "p.lehenaff@tudelft.nl",
     description = "A quantum circuit transformation and manipulation tool",
