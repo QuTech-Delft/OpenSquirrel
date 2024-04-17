a1cfd4a3fe8370c6a467e2d58fa621e4b9ad28b7
Juan Boschero <juan.boschero@tno.nl>
2024-04-17 14:30:47 +0200

    Revert quirrel_ir_from_string

--- opensquirrel/parsing/antlr/squirrel_ir_from_string.py
+++ opensquirrel/parsing/antlr/squirrel_ir_from_string.py
@@ -23,8 +23,8 @@ def antlr_tree_from_string(s: str):
     return parser.prog()
 
 
-def type_check_antlr_tree(tree, gate_set, gate_aliases, measurement_set):
-    type_checker = TypeChecker(gate_set, gate_aliases, measurement_set)
+def type_check_antlr_tree(tree, gate_set: list, gate_aliases: dict):
+    type_checker = TypeChecker(gate_set, gate_aliases)
     type_checker.visit(tree)  # FIXME: return error instead of throwing?
 
 
@@ -33,7 +33,7 @@ def check_qubit_ranges_of_antlr_tree(tree):
     qubit_range_checker.visit(tree)  # FIXME: return error instead of throwing?
 
 
-def squirrel_ir_from_string(s: str, gate_set, gate_aliases, measurement_set):
+def squirrel_ir_from_string(s: str, gate_set: list, gate_aliases: dict) -> SquirrelIR:
     """
     ANTLR parsing entrypoint.
     Performs type checking based on provided gate semantics and check that the qubit indices are valid.
@@ -42,26 +42,16 @@ def squirrel_ir_from_string(s: str, gate_set, gate_aliases, measurement_set):
     Args:
         gate_set: The set of supported gate semantics.
         gate_aliases: Dictionary mapping extra gate names to their semantic.
-        measurement_set: The set of supported measurement semantics.
 
     Returns:
         A corresponding SquirrelIR object. Throws in case of parsing error.
     """
     tree = antlr_tree_from_string(s)
 
-    type_check_antlr_tree(
-        tree,
-        gate_set=gate_set,
-        gate_aliases=gate_aliases,
-        measurement_set=measurement_set,
-    )
+    type_check_antlr_tree(tree, gate_set=gate_set, gate_aliases=gate_aliases)
 
     check_qubit_ranges_of_antlr_tree(tree)
 
-    squirrel_ir_creator = SquirrelIRCreator(
-        gate_set=gate_set,
-        gate_aliases=gate_aliases,
-        measurement_set=measurement_set,
-    )
+    squirrel_ir_creator = SquirrelIRCreator(gate_set=gate_set, gate_aliases=gate_aliases)
 
     return squirrel_ir_creator.visit(tree)
