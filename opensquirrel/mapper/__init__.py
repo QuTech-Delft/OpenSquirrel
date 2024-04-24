8f77b1e6b46c37ad2cb88894e9dfed8433eff14e
Stan van der Linde <stan.vanderlinde@tno.nl>
2024-04-24 15:41:22 +0200

    Bugfix in __init__.py

--- opensquirrel/mapper/__init__.py
+++ opensquirrel/mapper/__init__.py
@@ -1,4 +1,4 @@
-from opensquirrel.mapper.general_mapper import map, Mapper
-from opensquirrel.mapper.simple_mappers import IdentityMapper, HardcodedMapper
+from opensquirrel.mapper.general_mapper import Mapper, map_qubits
+from opensquirrel.mapper.simple_mappers import HardcodedMapper, IdentityMapper
 
-__all__ = ["map", "Mapper", "IdentityMapper", "HardcodedMapper"]
+__all__ = ["map_qubits", "Mapper", "IdentityMapper", "HardcodedMapper"]
