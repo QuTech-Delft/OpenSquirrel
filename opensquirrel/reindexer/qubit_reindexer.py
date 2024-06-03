a25c92c84275326cdbdfe282db252c26794267e3
Stan van der Linde <stan.vanderlinde@tno.nl>
2024-06-03 15:42:12 +0200

    Fix type hints reindexer

--- opensquirrel/reindexer/qubit_reindexer.py
+++ opensquirrel/reindexer/qubit_reindexer.py
@@ -1,4 +1,6 @@
-from typing import List
+from __future__ import annotations
+
+from collections.abc import Iterable
 
 from opensquirrel.circuit import Circuit
 from opensquirrel.ir import (
@@ -30,28 +32,28 @@ class _QubitReindexer(IRVisitor):
          - Qubit(index=3) becomes Qubit(index=0).
     """
 
-    def __init__(self, qubit_indices: List[int]):
+    def __init__(self, qubit_indices: list[int]) -> None:
         self.qubit_indices = qubit_indices
 
-    def visit_measure(self, measure: Measure):
+    def visit_measure(self, measure: Measure) -> Measure:
         return Measure(qubit=Qubit(self.qubit_indices.index(measure.qubit.index)), axis=measure.axis)
 
-    def visit_bloch_sphere_rotation(self, g: BlochSphereRotation):
+    def visit_bloch_sphere_rotation(self, g: BlochSphereRotation) -> BlochSphereRotation:
         return BlochSphereRotation(
             qubit=Qubit(self.qubit_indices.index(g.qubit.index)), angle=g.angle, axis=g.axis, phase=g.phase
         )
 
-    def visit_matrix_gate(self, g: MatrixGate):
+    def visit_matrix_gate(self, g: MatrixGate) -> MatrixGate:
         reindexed_operands = [Qubit(self.qubit_indices.index(op.index)) for op in g.operands]
         return MatrixGate(matrix=g.matrix, operands=reindexed_operands)
 
-    def visit_controlled_gate(self, controlled_gate: ControlledGate):
+    def visit_controlled_gate(self, controlled_gate: ControlledGate) -> ControlledGate:
         control_qubit = Qubit(self.qubit_indices.index(controlled_gate.control_qubit.index))
         target_gate = controlled_gate.target_gate.accept(self)
         return ControlledGate(control_qubit=control_qubit, target_gate=target_gate)
 
 
-def get_reindexed_circuit(replacement_gates: List[Gate], qubit_indices: List[int]) -> Circuit:
+def get_reindexed_circuit(replacement_gates: Iterable[Gate], qubit_indices: list[int]) -> Circuit:
     qubit_reindexer = _QubitReindexer(qubit_indices)
     register_manager = RegisterManager(qubit_register_size=len(qubit_indices))
     replacement_ir = IR()
