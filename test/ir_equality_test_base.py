d4ebc9e07b073d8fc5cf30cf060f303d1350f2e2
Stan van der Linde <stan.vanderlinde@tno.nl>
2024-06-04 15:01:38 +0200

    Update tests

--- test/ir_equality_test_base.py
+++ test/ir_equality_test_base.py
@@ -28,3 +28,28 @@ class IREqualityTestBase(unittest.TestCase):
 
         if expected_circuit is not None:
             self.assertEqual(circuit, expected_circuit)
+
+
+def check_equivalence_up_to_global_phase(matrix_a, matrix_b):
+    assert are_matrices_equivalent_up_to_global_phase(matrix_a, matrix_b)
+
+
+def modify_circuit_and_check(circuit, action, expected_circuit=None):
+    """
+    Checks whether the action preserves:
+    - the number of qubits,
+    - the qubit register name(s),
+    - the circuit matrix up to a global phase factor.
+    """
+    # Store matrix before decompositions.
+    expected_matrix = circuit_matrix_calculator.get_circuit_matrix(circuit)
+
+    action(circuit)
+
+    # Get matrix after decompositions.
+    actual_matrix = circuit_matrix_calculator.get_circuit_matrix(circuit)
+
+    check_equivalence_up_to_global_phase(actual_matrix, expected_matrix)
+
+    if expected_circuit is not None:
+        assert circuit == expected_circuit
--- test/test_circuit_matrix_calculator.py
+++ test/test_circuit_matrix_calculator.py
@@ -1,195 +1,168 @@
-import unittest
+import math
+
+import numpy as np
 
 from opensquirrel import circuit_matrix_calculator
 from opensquirrel.circuit import Circuit
-from opensquirrel.default_gates import *
+from opensquirrel.default_gates import CNOT, H, X
 from opensquirrel.ir import IR, Qubit
 from opensquirrel.register_manager import RegisterManager
 
 
-class CircuitMatrixCalculatorTest(unittest.TestCase):
-    def test_hadamard(self):
-        register_manager = RegisterManager(qubit_register_size=1)
-        ir = IR()
-        ir.add_gate(H(Qubit(0)))
-        circuit = Circuit(register_manager, ir)
+def test_hadamard() -> None:
+    register_manager = RegisterManager(qubit_register_size=1)
+    ir = IR()
+    ir.add_gate(H(Qubit(0)))
+    circuit = Circuit(register_manager, ir)
 
-        self.assertTrue(
-            np.allclose(
-                circuit_matrix_calculator.get_circuit_matrix(circuit),
-                math.sqrt(0.5)
-                * np.array(
-                    [
-                        [1, 1],
-                        [1, -1],
-                    ]
-                ),
-            )
-        )
-
-    def test_double_hadamard(self):
-        register_manager = RegisterManager(qubit_register_size=1)
-        ir = IR()
-        ir.add_gate(H(Qubit(0)))
-        ir.add_gate(H(Qubit(0)))
-        circuit = Circuit(register_manager, ir)
-
-        self.assertTrue(np.allclose(circuit_matrix_calculator.get_circuit_matrix(circuit), np.eye(2)))
-
-    def test_triple_hadamard(self):
-        register_manager = RegisterManager(qubit_register_size=1)
-        ir = IR()
-        ir.add_gate(H(Qubit(0)))
-        ir.add_gate(H(Qubit(0)))
-        ir.add_gate(H(Qubit(0)))
-        circuit = Circuit(register_manager, ir)
-
-        self.assertTrue(
-            np.allclose(
-                circuit_matrix_calculator.get_circuit_matrix(circuit),
-                math.sqrt(0.5)
-                * np.array(
-                    [
-                        [1, 1],
-                        [1, -1],
-                    ]
-                ),
-            )
-        )
-
-    def test_hadamard_x(self):
-        register_manager = RegisterManager(qubit_register_size=2)
-        ir = IR()
-        ir.add_gate(H(Qubit(0)))
-        ir.add_gate(X(Qubit(1)))
-        circuit = Circuit(register_manager, ir)
-
-        self.assertTrue(
-            np.allclose(
-                circuit_matrix_calculator.get_circuit_matrix(circuit),
-                math.sqrt(0.5)
-                * np.array(
-                    [
-                        [0, 0, 1, 1],
-                        [0, 0, 1, -1],
-                        [1, 1, 0, 0],
-                        [1, -1, 0, 0],
-                    ]
-                ),
-            )
-        )
-
-    def test_x_hadamard(self):
-        register_manager = RegisterManager(qubit_register_size=2)
-        ir = IR()
-        ir.add_gate(H(Qubit(1)))
-        ir.add_gate(X(Qubit(0)))
-        circuit = Circuit(register_manager, ir)
-
-        self.assertTrue(
-            np.allclose(
-                circuit_matrix_calculator.get_circuit_matrix(circuit),
-                math.sqrt(0.5)
-                * np.array(
-                    [
-                        [0, 1, 0, 1],
-                        [1, 0, 1, 0],
-                        [0, 1, 0, -1],
-                        [1, 0, -1, 0],
-                    ]
-                ),
-            )
-        )
-
-    def test_cnot(self):
-        register_manager = RegisterManager(qubit_register_size=2)
-        ir = IR()
-        ir.add_gate(CNOT(Qubit(1), Qubit(0)))
-        circuit = Circuit(register_manager, ir)
-
-        self.assertTrue(
-            np.allclose(
-                circuit_matrix_calculator.get_circuit_matrix(circuit),
-                np.array(
-                    [
-                        [1, 0, 0, 0],
-                        [0, 1, 0, 0],
-                        [0, 0, 0, 1],
-                        [0, 0, 1, 0],
-                    ]
-                ),
-            )
-        )
-
-    def test_cnot_reversed(self):
-        register_manager = RegisterManager(qubit_register_size=2)
-        ir = IR()
-        ir.add_gate(CNOT(Qubit(0), Qubit(1)))
-        circuit = Circuit(register_manager, ir)
-
-        self.assertTrue(
-            np.allclose(
-                circuit_matrix_calculator.get_circuit_matrix(circuit),
-                np.array(
-                    [
-                        [1, 0, 0, 0],
-                        [0, 0, 0, 1],
-                        [0, 0, 1, 0],
-                        [0, 1, 0, 0],
-                    ]
-                ),
-            )
-        )
-
-    def test_hadamard_cnot(self):
-        register_manager = RegisterManager(qubit_register_size=2)
-        ir = IR()
-        ir.add_gate(H(Qubit(0)))
-        ir.add_gate(CNOT(Qubit(0), Qubit(1)))
-        circuit = Circuit(register_manager, ir)
-
-        self.assertTrue(
-            np.allclose(
-                circuit_matrix_calculator.get_circuit_matrix(circuit),
-                math.sqrt(0.5)
-                * np.array(
-                    [
-                        [1, 1, 0, 0],
-                        [0, 0, 1, -1],
-                        [0, 0, 1, 1],
-                        [1, -1, 0, 0],
-                    ]
-                ),
-            )
-        )
-
-    def test_hadamard_cnot_0_2(self):
-        register_manager = RegisterManager(qubit_register_size=3)
-        ir = IR()
-        ir.add_gate(H(Qubit(0)))
-        ir.add_gate(CNOT(Qubit(0), Qubit(2)))
-        circuit = Circuit(register_manager, ir)
-
-        print(circuit_matrix_calculator.get_circuit_matrix(circuit))
-        self.assertTrue(
-            np.allclose(
-                circuit_matrix_calculator.get_circuit_matrix(circuit),
-                math.sqrt(0.5)
-                * np.array(
-                    [
-                        [1, 1, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 1, -1, 0, 0],
-                        [0, 0, 1, 1, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 1, -1],
-                        [0, 0, 0, 0, 1, 1, 0, 0],
-                        [1, -1, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 1, 1],
-                        [0, 0, 1, -1, 0, 0, 0, 0],
-                    ]
-                ),
-            )
-        )
+    np.testing.assert_almost_equal(
+        circuit_matrix_calculator.get_circuit_matrix(circuit), math.sqrt(0.5) * np.array([[1, 1], [1, -1]])
+    )
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_double_hadamard() -> None:
+    register_manager = RegisterManager(qubit_register_size=1)
+    ir = IR()
+    ir.add_gate(H(Qubit(0)))
+    ir.add_gate(H(Qubit(0)))
+    circuit = Circuit(register_manager, ir)
+
+    np.testing.assert_almost_equal(circuit_matrix_calculator.get_circuit_matrix(circuit), np.eye(2))
+
+
+def test_triple_hadamard() -> None:
+    register_manager = RegisterManager(qubit_register_size=1)
+    ir = IR()
+    ir.add_gate(H(Qubit(0)))
+    ir.add_gate(H(Qubit(0)))
+    ir.add_gate(H(Qubit(0)))
+    circuit = Circuit(register_manager, ir)
+
+    np.testing.assert_almost_equal(
+        circuit_matrix_calculator.get_circuit_matrix(circuit), math.sqrt(0.5) * np.array([[1, 1], [1, -1]])
+    )
+
+
+def test_hadamard_x() -> None:
+    register_manager = RegisterManager(qubit_register_size=2)
+    ir = IR()
+    ir.add_gate(H(Qubit(0)))
+    ir.add_gate(X(Qubit(1)))
+    circuit = Circuit(register_manager, ir)
+
+    np.testing.assert_almost_equal(
+        circuit_matrix_calculator.get_circuit_matrix(circuit),
+        math.sqrt(0.5)
+        * np.array(
+            [
+                [0, 0, 1, 1],
+                [0, 0, 1, -1],
+                [1, 1, 0, 0],
+                [1, -1, 0, 0],
+            ]
+        ),
+    )
+
+
+def test_x_hadamard() -> None:
+    register_manager = RegisterManager(qubit_register_size=2)
+    ir = IR()
+    ir.add_gate(H(Qubit(1)))
+    ir.add_gate(X(Qubit(0)))
+    circuit = Circuit(register_manager, ir)
+
+    np.testing.assert_almost_equal(
+        circuit_matrix_calculator.get_circuit_matrix(circuit),
+        math.sqrt(0.5)
+        * np.array(
+            [
+                [0, 1, 0, 1],
+                [1, 0, 1, 0],
+                [0, 1, 0, -1],
+                [1, 0, -1, 0],
+            ]
+        ),
+    )
+
+
+def test_cnot() -> None:
+    register_manager = RegisterManager(qubit_register_size=2)
+    ir = IR()
+    ir.add_gate(CNOT(Qubit(1), Qubit(0)))
+    circuit = Circuit(register_manager, ir)
+
+    np.testing.assert_almost_equal(
+        circuit_matrix_calculator.get_circuit_matrix(circuit),
+        [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 1],
+            [0, 0, 1, 0],
+        ],
+    )
+
+
+def test_cnot_reversed() -> None:
+    register_manager = RegisterManager(qubit_register_size=2)
+    ir = IR()
+    ir.add_gate(CNOT(Qubit(0), Qubit(1)))
+    circuit = Circuit(register_manager, ir)
+
+    np.testing.assert_almost_equal(
+        circuit_matrix_calculator.get_circuit_matrix(circuit),
+        np.array(
+            [
+                [1, 0, 0, 0],
+                [0, 0, 0, 1],
+                [0, 0, 1, 0],
+                [0, 1, 0, 0],
+            ]
+        ),
+    )
+
+
+def test_hadamard_cnot() -> None:
+    register_manager = RegisterManager(qubit_register_size=2)
+    ir = IR()
+    ir.add_gate(H(Qubit(0)))
+    ir.add_gate(CNOT(Qubit(0), Qubit(1)))
+    circuit = Circuit(register_manager, ir)
+
+    np.testing.assert_almost_equal(
+        circuit_matrix_calculator.get_circuit_matrix(circuit),
+        math.sqrt(0.5)
+        * np.array(
+            [
+                [1, 1, 0, 0],
+                [0, 0, 1, -1],
+                [0, 0, 1, 1],
+                [1, -1, 0, 0],
+            ]
+        ),
+    )
+
+
+def test_hadamard_cnot_0_2() -> None:
+    register_manager = RegisterManager(qubit_register_size=3)
+    ir = IR()
+    ir.add_gate(H(Qubit(0)))
+    ir.add_gate(CNOT(Qubit(0), Qubit(2)))
+    circuit = Circuit(register_manager, ir)
+
+    print(circuit_matrix_calculator.get_circuit_matrix(circuit))
+    np.testing.assert_almost_equal(
+        circuit_matrix_calculator.get_circuit_matrix(circuit),
+        math.sqrt(0.5)
+        * np.array(
+            [
+                [1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, -1, 0, 0],
+                [0, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, -1],
+                [0, 0, 0, 0, 1, 1, 0, 0],
+                [1, -1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 1],
+                [0, 0, 1, -1, 0, 0, 0, 0],
+            ]
+        ),
+    )
