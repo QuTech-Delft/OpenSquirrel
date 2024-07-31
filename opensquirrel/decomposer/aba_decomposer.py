Fix formula for ABADecomposition

opensquirrel/decomposer/aba_decomposer.py
@@ -31,6 +31,18 @@ class ABADecomposer(Decomposer, ABC):
         self.index_a = self._gate_list.index(self.ra)
         self.index_b = self._gate_list.index(self.rb)
 
+    @staticmethod
+    def _find_non_used_index(axis_list: list[int]) -> int:
+        """Finds the index of the axis object that is not used in the decomposition.
+        For example, if ZYZ decomposition, the integer returned will be 0 (since it is X).
+        Parameters:
+            axis_list: list of the axis indices in use
+
+        Returns:
+            Index of the axis object that is not used in the decomposition.
+        """
+        return sorted(set(range(0, 3)) - set(axis_list))[0]
+
     def get_decomposition_angles(self, alpha: float, axis: AxisLike) -> tuple[float, float, float]:
         """Gives the angles used in the A-B-A decomposition of the Bloch sphere rotation
         characterized by a rotation around `axis` of angle `alpha`.
@@ -45,8 +57,10 @@ class ABADecomposer(Decomposer, ABC):
 
         """
         axis = Axis(axis)
-        a_axis_value = axis[self.index_a]
-        b_axis_value = axis[self.index_b]
+        a_axis_value = axis[self.index_a] + 0.0
+        b_axis_value = axis[self.index_b] + 0.0
+        c_axis_value = axis[self._find_non_used_index([self.index_a, self.index_b])] + 0.0
+
         if not (-math.pi + ATOL < alpha <= math.pi + ATOL):
             raise ValueError("angle needs to be normalized")
 
@@ -84,15 +98,16 @@ class ABADecomposer(Decomposer, ABC):
                 # This fixes float approximations like 1.0000000000002, which acos does not like.
                 acos_argument = max(min(acos_argument, 1.0), -1.0)
                 m = 2 * math.acos(acos_argument)
+                if math.pi - abs(m) > ATOL:
+                    m_sign = 2 * math.atan(c_axis_value / a_axis_value)
+                    m = math.copysign(m, m_sign)
+
+        is_sin_m_negative = self.index_a - self.index_b == -1 or self.index_a - self.index_b == 2
+        if is_sin_m_negative:
+            m = m * -1
 
         theta1 = (p + m) / 2
         theta3 = p - theta1
-
-        is_in_lower_half = math.copysign(math.sin(p), math.sin(p) * np.prod(axis)) <= ATOL
-        is_sin_m_negative = self.index_a - self.index_b == -1 or self.index_a - self.index_b == 2
-
-        if is_in_lower_half and is_sin_m_negative:
-            theta1, theta3 = theta3, theta1
 
         return theta1, theta2, theta3
 