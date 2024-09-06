7ad9ec5467683a638ac89ec8fa152f5ec7393cd7
Chris Elenbaas <chris.elenbaas@tno.nl>
2024-09-06 17:02:26 +0200

    Add tests for cQASM v1 exporter.

--- opensquirrel/exporter/cqasmv1_exporter.py
+++ opensquirrel/exporter/cqasmv1_exporter.py
@@ -1,74 +1,64 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
-from opensquirrel.default_gates import X, Z
-from opensquirrel.exceptions import ExporterError, UnsupportedGateError
-from opensquirrel.ir import BlochSphereRotation, ControlledGate, IRVisitor, MatrixGate, Measure, Qubit, Reset
+from opensquirrel.exceptions import UnsupportedGateError
+from opensquirrel.ir import Comment, Float, Gate, Int, IRVisitor, Measure, Qubit, Reset
 
 if TYPE_CHECKING:
     from opensquirrel.circuit import Circuit
+    from opensquirrel.register_manager import RegisterManager
 
 
 class _CQASMv1Creator(IRVisitor):
-    def _get_qubit_string(self, q: Qubit) -> str:
-        return f"{self.qubit_register_name}[{q.index}]"
+    # Precision used when writing out a float number
+    FLOAT_PRECISION = 8
 
-    def __init__(self, qubit_register_name: str) -> None:
-        self.qubit_register_name = qubit_register_name
-        self.cqasmv1_string = "version 1.0\n\n"
+    def __init__(self, register_manager: RegisterManager) -> None:
+        self.register_manager = register_manager
+        qubit_register_size = self.register_manager.get_qubit_register_size()
+        self.cqasmv1_string = "version 1.0\n\n{}\n\n".format(
+            f"qubits {qubit_register_size}" if qubit_register_size > 0 else ""
+        )
 
-    def visit_bloch_sphere_rotation(self, g: BlochSphereRotation) -> None:
-        # Note that when adding a rotation gate to the Quantify-scheduler Schedule,
-        # there exists an ambiguity with how Quantify-scheduler will store an angle of 180 degrees.
-        # Depending on the system the angle may be stored as either 180 or -180 degrees.
-        # This ambiguity has no physical consequences, but may cause the exporter test fail.
-        # if g is recognized:
-        #     ...
-        #     return
-        #
-        # raise UnsupportedGateError(g)
+    def visit_qubit(self, qubit: Qubit) -> str:
+        qubit_register_name = self.register_manager.get_qubit_register_name()
+        return f"{qubit_register_name}[{qubit.index}]"
 
-    def visit_matrix_gate(self, g: MatrixGate) -> None:
-        raise UnsupportedGateError(g)
+    def visit_int(self, i: Int) -> str:
+        return f"{i.value}"
 
-    def visit_controlled_gate(self, g: ControlledGate) -> None:
-        if not isinstance(g.target_gate, BlochSphereRotation):
-            raise UnsupportedGateError(g)
+    def visit_float(self, f: Float) -> str:
+        return f"{f.value:.{self.FLOAT_PRECISION}}"
 
-        if g.target_gate == X(g.target_gate.qubit):
-            # check if anything special needs to be done.
-                    #self._get_qubit_string(g.control_qubit),
-                    #self._get_qubit_string(g.target_gate.qubit),
-            # return
-            return
+    def visit_measure(self, measure: Measure) -> None:
+        qubit_argument = measure.arguments[0].accept(self)  # type: ignore[index]
+        self.cqasmv1_string += f"{measure.name}_z {qubit_argument}\n"
 
-        if g.target_gate == Z(g.target_gate.qubit):
-            # check if anything special needs to be done.
-            # self._get_qubit_string(g.control_qubit),
-            # self._get_qubit_string(g.target_gate.qubit),
-            # return
-            return
+    def visit_reset(self, reset: Reset) -> None:
+        qubit_argument = reset.arguments[0].accept(self)  # type: ignore[index]
+        self.cqasmv1_string += f"prep_z {qubit_argument}\n"
 
-        raise UnsupportedGateError(g)
+    def visit_gate(self, gate: Gate) -> None:
+        gate_name = gate.name.lower()
+        if gate.is_anonymous:
+            raise UnsupportedGateError(gate)
+        params = ""
+        if any(not isinstance(arg, Qubit) for arg in gate.arguments):  # type: ignore[union-attr]
+            params = [arg.accept(self) for arg in gate.arguments if
+                      not isinstance(arg, Qubit)]  # type: ignore[union-attr]
+        qubit_args = (arg.accept(self) for arg in gate.arguments if isinstance(arg, Qubit))  # type: ignore[union-attr]
+        self.cqasmv1_string += "{} {}{}\n".format(
+            gate_name, ", ".join(qubit_args), ", " + ", ".join(params) if params else ""
+        )
 
-    def visit_measure(self, g: Measure) -> None:
-        # remove the bit variable
-        pass
-
-    def visit_reset(self, g: Reset) -> Any:
-        # turn into prep_z?
-        pass
+    def visit_comment(self, comment: Comment) -> None:
+        self.cqasmv1_string += f"\n/* {comment.str} */\n\n"
 
 
 def export(circuit: Circuit) -> str:
+    cqasmv1_creator = _CQASMv1Creator(circuit.register_manager)
 
-    cqasmv1_creator = _CQASMv1Creator(circuit.qubit_register_name)
-    try:
-        circuit.ir.accept(cqasmv1_creator)
-    except UnsupportedGateError as e:
-        msg = (
-            f"cannot export circuit: {e}."
-        )
-        raise ExporterError(msg) from e
-    return cqasmv1_creator.cqasmv1_string
+    circuit.ir.accept(cqasmv1_creator)
+
+    return cqasmv1_creator.cqasmv1_string.rstrip() + "\n" # remove all trailing lines and leave only one
---
+++ test/exporter/test_cqasmv1_exporter.py
@@ -0,0 +1,156 @@
+import numpy as np
+import pytest
+
+from opensquirrel import CircuitBuilder
+from opensquirrel.exceptions import UnsupportedGateError
+from opensquirrel.exporter.export_format import ExportFormat
+from opensquirrel.ir import Bit, BlochSphereRotation, ControlledGate, Float, Gate, MatrixGate, Qubit
+
+
+def test_qubit_statement() -> None:
+    builder = CircuitBuilder(3)
+    qc = builder.to_circuit()
+    cqasm_v1_string = qc.export(fmt=ExportFormat.CQASM_V1)
+    assert cqasm_v1_string == """version 1.0
+
+qubits 3
+"""
+
+
+def test_circuit_to_string_after_circuit_modification() -> None:
+    builder = CircuitBuilder(3)
+    qc = builder.to_circuit()
+    cqasm_v1_string = qc.export(fmt=ExportFormat.CQASM_V1)
+    assert cqasm_v1_string == """version 1.0
+
+qubits 3
+"""
+
+    builder.H(Qubit(0))
+    builder.CNOT(Qubit(0), Qubit(1))
+    qc = builder.to_circuit()
+    cqasm_v1_string = qc.export(fmt=ExportFormat.CQASM_V1)
+    assert cqasm_v1_string == """version 1.0
+
+qubits 3
+
+h q[0]
+cnot q[0], q[1]
+"""
+
+
+def test_float_precision() -> None:
+    builder = CircuitBuilder(3)
+    builder.Rx(Qubit(0), Float(1.6546514861321684321654))
+    qc = builder.to_circuit()
+    cqasm_v1_string = qc.export(fmt=ExportFormat.CQASM_V1)
+    assert cqasm_v1_string == """version 1.0
+
+qubits 3
+
+rx q[0], 1.6546515
+"""
+
+
+def test_measure() -> None:
+    builder = CircuitBuilder(1, 1)
+    builder.H(Qubit(0))
+    builder.measure(Qubit(0), Bit(0))
+    qc = builder.to_circuit()
+    cqasm_v1_string = qc.export(fmt=ExportFormat.CQASM_V1)
+    assert cqasm_v1_string == """version 1.0
+
+qubits 1
+
+h q[0]
+measure_z q[0]
+"""
+
+
+def test_reset() -> None:
+    builder = CircuitBuilder(1, 1)
+    builder.H(Qubit(0))
+    builder.reset(Qubit(0))
+    qc = builder.to_circuit()
+    cqasm_v1_string = qc.export(fmt=ExportFormat.CQASM_V1)
+    assert cqasm_v1_string == """version 1.0
+
+qubits 1
+
+h q[0]
+prep_z q[0]
+"""
+
+
+def test_all_supported_gates() -> None:
+    builder = CircuitBuilder(2,2)
+    builder.reset(Qubit(0)).reset(Qubit(1))
+    builder.X(Qubit(0)).Y(Qubit(0)).Z(Qubit(0))
+    builder.Rx(Qubit(0), Float(1.234)).Ry(Qubit(0), Float(-1.234)).Rz(Qubit(0), Float(1.234))
+    builder.X90(Qubit(0)).Y90(Qubit(0))
+    builder.mX90(Qubit(0)).mY90(Qubit(0))
+    builder.S(Qubit(0)).Sdag(Qubit(0)).T(Qubit(0)).Tdag(Qubit(0))
+    builder.CZ(Qubit(0), Qubit(1)).CNOT(Qubit(1), Qubit(0))
+    builder.measure(Qubit(0), Bit(0)).measure(Qubit(1), Bit(1))
+    qc = builder.to_circuit()
+    cqasm_v1_string = qc.export(fmt=ExportFormat.CQASM_V1)
+    assert cqasm_v1_string == """version 1.0
+
+qubits 2
+
+prep_z q[0]
+prep_z q[1]
+x q[0]
+y q[0]
+z q[0]
+rx q[0], 1.234
+ry q[0], -1.234
+rz q[0], 1.234
+x90 q[0]
+y90 q[0]
+mx90 q[0]
+my90 q[0]
+s q[0]
+sdag q[0]
+t q[0]
+tdag q[0]
+cz q[0], q[1]
+cnot q[1], q[0]
+measure_z q[0]
+measure_z q[1]
+"""
+
+
+@pytest.mark.parametrize(
+    "gate",
+    [
+        BlochSphereRotation(Qubit(0), axis=(1, 1, 1), angle=1.23),
+        ControlledGate(Qubit(0), BlochSphereRotation(Qubit(1), axis=(1, 1, 1), angle=1.23)),
+        MatrixGate(np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]), [Qubit(0), Qubit(1)]),
+    ]
+)
+def test_anonymous_gates(gate: Gate) -> None:
+    builder = CircuitBuilder(2)
+    builder.ir.add_gate(gate)
+    with pytest.raises(UnsupportedGateError, match="not supported"):  # noqa: PT012
+        qc = builder.to_circuit()
+        qc.export(fmt=ExportFormat.CQASM_V1)
+
+
+def test_comment() -> None:
+    builder = CircuitBuilder(3)
+    builder.H(Qubit(0))
+    builder.comment("My comment")
+    builder.Rx(Qubit(0), Float(1.234))
+    qc = builder.to_circuit()
+    cqasm_v1_string = qc.export(fmt=ExportFormat.CQASM_V1)
+    assert cqasm_v1_string == """version 1.0
+
+qubits 3
+
+h q[0]
+
+/* My comment */
+
+rx q[0], 1.234
+"""
