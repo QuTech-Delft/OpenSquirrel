ceed8ee40b8138b95ec4ae0e12a664c8f6a7ac4d
Stan van der Linde <stan.vanderlinde@tno.nl>
2024-09-20 14:09:11 +0200

    Use SupportsFloat in writer

--- opensquirrel/writer/writer.py
+++ opensquirrel/writer/writer.py
@@ -1,9 +1,11 @@
 import inspect
-from typing import SupportsInt
+from typing import SupportsInt, SupportsFloat
 
 from opensquirrel.circuit import Circuit
-from opensquirrel.ir import Bit, Comment, Float, Gate, Int, IRVisitor, Measure, Qubit, Reset
+from opensquirrel.ir import Bit, Comment, Gate, Int, Float, IRVisitor, Measure, Qubit, Reset
 from opensquirrel.register_manager import RegisterManager
+
+
 
 
 class _WriterImpl(IRVisitor):
@@ -33,7 +35,8 @@ class _WriterImpl(IRVisitor):
         i = Int(i)
         return f"{i.value}"
 
-    def visit_float(self, f: Float) -> str:
+    def visit_float(self, f: SupportsFloat) -> str:
+        f = Float(f)
         return f"{f.value:.{self.FLOAT_PRECISION}}"
 
     def visit_measure(self, measure: Measure) -> None:
