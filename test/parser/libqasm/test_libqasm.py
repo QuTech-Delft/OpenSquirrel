105ddc35eecee79be1160eeb498f4fdc905911df
Roberto Turrado Camblor <rturrado@gmail.com>
2024-11-08 12:54:04 +0100

    Apply suggestions from code review.
    Co-authored-by: Chris Elenbaas <67630508+elenbaasc@users.noreply.github.com>

--- test/parser/libqasm/test_libqasm.py
+++ test/parser/libqasm/test_libqasm.py
@@ -128,7 +128,7 @@ def test_simplest(parser: Parser, circuit_string: str, expected_output: str) -> 
     [
         (
             "version 3.0; qubit q; inv.X q",
-            [BlochSphereRotation(qubit=0, axis=(1, 0, 0), angle=math.pi * -1, phase=math.pi / 2)],
+            [BlochSphereRotation(qubit=0, axis=(1, 0, 0), angle=math.pi * -1, phase=math.pi / 2 * -1)],
         ),
         (
             "version 3.0; qubit q; pow(2).Rx(pi) q",
@@ -136,11 +136,11 @@ def test_simplest(parser: Parser, circuit_string: str, expected_output: str) -> 
         ),
         (
             "version 3.0; qubit q; pow(2).inv.X q",
-            [BlochSphereRotation(qubit=0, axis=(1, 0, 0), angle=math.pi * -2, phase=math.pi / 2)],
+            [BlochSphereRotation(qubit=0, axis=(1, 0, 0), angle=math.pi * -2, phase=math.pi / 2 * -2)],
         ),
         (
             "version 3.0; qubit[2] q; ctrl.pow(2).inv.X q[0], q[1]",
-            [ControlledGate(0, BlochSphereRotation(qubit=1, axis=(1, 0, 0), angle=math.pi * -2, phase=math.pi / 2))],
+            [ControlledGate(0, BlochSphereRotation(qubit=1, axis=(1, 0, 0), angle=math.pi * -2, phase=math.pi / 2 * -2))],
         ),
     ],
     ids=["inv", "pow_2_Rx", "pow_2_inv", "ctrl_pow_2_inv"],
