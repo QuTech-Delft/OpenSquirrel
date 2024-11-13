9ff9d89e544bf20a8d3fd60606051b9cd42d24d2
Juan Boschero <juan.boschero@tno.nl>
2024-11-13 19:35:42 +0100

    Modify flatten_irregular_list

--- opensquirrel/utils/list.py
+++ opensquirrel/utils/list.py
@@ -10,7 +10,4 @@ def flatten_list(list_to_flatten: list[list[Any]]) -> list[Any]:
 
 
 def flatten_irregular_list(list_to_flatten: list[Any]) -> list[Any]:
-    if isinstance(list_to_flatten, list):
-        return [element for i in list_to_flatten for element in flatten_irregular_list(i)]
-
-    return [list_to_flatten]
+    return [element for i in list_to_flatten for element in flatten_irregular_list(i)]
