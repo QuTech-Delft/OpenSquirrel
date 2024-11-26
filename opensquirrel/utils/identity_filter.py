f804b809effbcc923c52b67be2a43b389fec8f8b
Juan Carlos Boschero <juanboschero1998@gmail.com>
2024-11-26 08:34:22 +0100

    Update opensquirrel/utils/identity_filter.py
    Co-authored-by: Roberto Turrado Camblor <rturrado@gmail.com>

--- opensquirrel/utils/identity_filter.py
+++ opensquirrel/utils/identity_filter.py
@@ -16,6 +16,6 @@ def filter_out_identities(gates: Iterable[Gate]) -> list[Gate]:
     return [gate for gate in gates if not gate.is_identity()]
 
 
-def is_matrix_identity(matrix: ArrayLike) -> bool:
+def is_identity_matrix(matrix: ArrayLike) -> bool:
     norm_matrix = np.abs(matrix)
     return np.allclose(norm_matrix, np.eye(norm_matrix.shape[0]), atol=ATOL)
