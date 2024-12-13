fff2a5e1766223514b11e62e1b22aeec9f6ff7fa
Stan van der Linde <stan.vanderlinde@tno.nl>
2024-12-13 16:52:47 +0100

    Add tests

--- opensquirrel/utils/bit_register_mapping.py
+++ opensquirrel/utils/bit_register_mapping.py
@@ -1,9 +1,12 @@
 """This module contains the ``BitStringMapping`` class."""
+
 from __future__ import annotations
 
 from collections import defaultdict
+from typing import TYPE_CHECKING
 
-from opensquirrel.ir import Measure
+if TYPE_CHECKING:
+    from opensquirrel.ir import Measure
 
 
 class BitStringMapping:
@@ -11,12 +14,12 @@ class BitStringMapping:
 
     def __init__(self) -> None:
         """Init of the ``BitStringMapping``."""
-        self._mapping = defaultdict(list)
-        self._bit_allocation = {}
+        self._mapping: defaultdict[int, list[int]] = defaultdict(list)
+        self._bit_allocation: dict[int, int] = {}
 
     def add_measure(self, g: Measure) -> None:
         """Add a acquisition_channel and bitstring_index of `g` to the mapping.
-        
+
         Args:
             g: measurement to add the acquisition_channel and bitstring_index from.
         """
@@ -28,28 +31,32 @@ class BitStringMapping:
 
     def get_last_added_acq(self) -> tuple[int, int]:
         """Get the last added acquisition_channel corresponding acquisition_index.
-        
+
         Especially useful when exporting to ``quantify_scheduler``, since not only the
         final acquisition_channel-acquisition_index pairs are needed, but also all
         intermediate pairs.
-        
+
         Returns:
             Tuple containing the last added acquisition_channel and corresponding
             acquisition_index.
         """
-        acq_channel = next(reversed(self._mapping))
+        try:
+            acq_channel = next(reversed(self._mapping))
+        except StopIteration as e:
+            msg = "BitStringMapping is empty, so there is no acq to get"
+            raise ValueError(msg) from e
         acq_idx = len(self._mapping[acq_channel]) - 1
         return acq_channel, acq_idx
 
     def to_export_format(self) -> dict[str, dict[str, int]]:
         """Export to json format.
-        
+
         For each bit, only the last executed measurement remains in the mapping.
-        
+
         Returns:
             Dictionary with the following format:
             {
-            "<acquisition_channel>": 
+            "<acquisition_channel>":
             {
                 "<acquisition_index>": <bitstring_index>,
                 "<acquisition_index>": <bitstring_index>,
@@ -58,33 +65,9 @@ class BitStringMapping:
             ...
             }
         """
-        json_mapping = defaultdict(dict)
+        json_mapping: defaultdict[str, dict[str, int]] = defaultdict(dict)
         for acq_channel, bit_indices in self._mapping.items():
             for acq_idx, bit_idx in enumerate(bit_indices):
-                if self.bit_allocation[bit_idx] == acq_channel:
+                if self._bit_allocation[bit_idx] == acq_channel:
                     json_mapping[str(acq_channel)][str(acq_idx)] = bit_idx
         return dict(json_mapping)
-
-
-if __name__ == "__main__":
-    import math
-
-    from opensquirrel import CircuitBuilder
-    builder = CircuitBuilder(3, 4)
-    builder.Rx(0, math.pi)
-    builder.measure(0, 0)
-    builder.Rx(1, math.pi)
-    builder.measure(1, 1)
-    builder.Rx(2, math.pi)
-    builder.measure(2, 0)
-    builder.Rx(0, math.pi)
-    builder.measure(0, 2)
-
-    circuit = builder.to_circuit()
-
-    bit_string_mapping = BitStringMapping()
-    for instruction in circuit.ir.statements:
-        if isinstance(instruction, Measure):
-            bit_string_mapping.add_measure(instruction)
-
-    print(bit_string_mapping.to_export_format())
---
+++ test/utils/test_bit_register_mapping.py
@@ -0,0 +1,71 @@
+import math
+from collections.abc import Iterable
+
+import pytest
+
+from opensquirrel import CircuitBuilder
+from opensquirrel.default_instructions import measure
+from opensquirrel.ir import Measure
+from opensquirrel.utils.bit_register_mapping import BitStringMapping
+
+
+@pytest.fixture(name="bit_string_mapping")
+def bit_string_mapping_fixture() -> BitStringMapping:
+    return BitStringMapping()
+
+
+@pytest.mark.parametrize(
+    ("measurements", "expected_mapping", "expected_bit_allocation"),
+    [
+        ([measure(0, 0)], {0: [0]}, {0: 0}),
+        ([measure(0, 0), measure(1, 0)], {0: [0], 1: [0]}, {0: 1}),
+        ([measure(0, 1), measure(0, 0)], {0: [1, 0]}, {0: 0, 1: 0}),
+    ],
+)
+def test_add_measure(
+    bit_string_mapping: BitStringMapping,
+    measurements: Iterable[Measure],
+    expected_mapping: dict[int, list[int]],
+    expected_bit_allocation: dict[int, int],
+) -> None:
+    for measurement in measurements:
+        bit_string_mapping.add_measure(measurement)
+    assert bit_string_mapping._mapping == expected_mapping
+    assert bit_string_mapping._bit_allocation == expected_bit_allocation
+
+
+def test_get_last_added_acq(bit_string_mapping: BitStringMapping) -> None:
+    bit_string_mapping.add_measure(measure(0, 0))
+    assert bit_string_mapping.get_last_added_acq() == (0, 0)
+    bit_string_mapping.add_measure(measure(0, 0))
+    assert bit_string_mapping.get_last_added_acq() == (0, 1)
+    bit_string_mapping.add_measure(measure(1, 0))
+    assert bit_string_mapping.get_last_added_acq() == (1, 0)
+    bit_string_mapping.add_measure(measure(2, 1))
+    assert bit_string_mapping.get_last_added_acq() == (2, 0)
+
+
+def test_get_last_added_acq_error(bit_string_mapping: BitStringMapping) -> None:
+    with pytest.raises(ValueError, match="BitStringMapping is empty, so there is no acq to get"):
+        bit_string_mapping.get_last_added_acq()
+
+
+def test_to_export_format(bit_string_mapping: BitStringMapping) -> None:
+    builder = CircuitBuilder(3, 4)
+    builder.Rx(0, math.pi)
+    builder.measure(0, 0)
+    builder.Rx(1, math.pi)
+    builder.measure(1, 1)
+    builder.Rx(2, math.pi)
+    builder.measure(2, 0)
+    builder.Rx(0, math.pi)
+    builder.measure(0, 2)
+
+    circuit = builder.to_circuit()
+
+    for instruction in circuit.ir.statements:
+        if isinstance(instruction, Measure):
+            bit_string_mapping.add_measure(instruction)
+
+    expected_output = {"0": {"1": 2}, "1": {"0": 1}, "2": {"0": 0}}
+    assert bit_string_mapping.to_export_format() == expected_output
