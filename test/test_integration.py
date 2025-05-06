a331ed19d35d1fb8da98d3b68c97b90d8109c6e4
Chris Elenbaas <chris.elenbaas@tno.nl>
2025-05-06 11:35:08 +0200

    Add additional ALAP tests to test_integration (WIP).

--- test/test_integration.py
+++ test/test_integration.py
@@ -593,6 +593,95 @@ b = measure q
         fig.savefig("example.png")
         fig.show()
 
+def test_hectoqubit_alap2() -> None:
+    # This test is used for the implementation of the ALAP scheduling
+    # in the quantify-scheduler Exporter. It should be rewritten to a standard test
+    # before this feature branch is merged into develop.
+    qc = Circuit.from_string(
+        """version 3.0
+
+qubit[5]  q
+bit[2]  b
+
+init q
+barrier q
+X q[3]
+barrier q[3,4]
+X q[4]
+barrier q[3,4]
+X q[4]
+b[0,1] = measure q[3,4]
+        """
+    )
+
+    # Decompose 2-qubit gates to a decomposition where the 2-qubit interactions are captured by CNOT gates
+    # qc.decompose(decomposer=CZDecomposer())
+
+    # Merge single-qubit gate
+    # qc.merge(merger=SingleQubitGatesMerger())
+
+    # Decompose single-qubit gates to HectoQubit backend native gates with the XYX decomposer
+    # qc.decompose(decomposer=XYXDecomposer())
+
+    if importlib.util.find_spec("quantify_scheduler") is None:
+        with pytest.raises(
+            Exception,
+            match="quantify-scheduler is not installed, or cannot be installed on your system",
+        ):
+            qc.export(fmt=ExportFormat.QUANTIFY_SCHEDULER)
+    else:
+        exported_schedule, _ = qc.export(fmt=ExportFormat.QUANTIFY_SCHEDULER)
+
+        fig, _ = exported_schedule.plot_circuit_diagram(figsize=(20, 6))
+        fig.set_dpi(300)
+        fig.savefig("example.png")
+        fig.show()
+
+def test_hectoqubit_alap3() -> None:
+    # This test is used for the implementation of the ALAP scheduling
+    # in the quantify-scheduler Exporter. It should be rewritten to a standard test
+    # before this feature branch is merged into develop.
+    qc = Circuit.from_string(
+        """version 3.0
+
+qubit[5]  q
+bit[5]  b
+
+init q
+barrier q
+mY90 q[2]
+CNOT q[2], q[0]
+CNOT q[2], q[1]
+CNOT q[2], q[3]
+CNOT q[2], q[4]
+barrier q
+b[0:4] = measure q[0:4]
+        """
+    )
+
+    # Decompose 2-qubit gates to a decomposition where the 2-qubit interactions are captured by CNOT gates
+    # qc.decompose(decomposer=CZDecomposer())
+
+    # Merge single-qubit gate
+    # qc.merge(merger=SingleQubitGatesMerger())
+
+    # Decompose single-qubit gates to HectoQubit backend native gates with the XYX decomposer
+    # qc.decompose(decomposer=XYXDecomposer())
+
+    if importlib.util.find_spec("quantify_scheduler") is None:
+        with pytest.raises(
+            Exception,
+            match="quantify-scheduler is not installed, or cannot be installed on your system",
+        ):
+            qc.export(fmt=ExportFormat.QUANTIFY_SCHEDULER)
+    else:
+        exported_schedule, _ = qc.export(fmt=ExportFormat.QUANTIFY_SCHEDULER)
+
+        fig, _ = exported_schedule.plot_circuit_diagram(figsize=(20, 6))
+        fig.set_dpi(300)
+        fig.savefig("example.png")
+        fig.show()
+
 
 def test_hectoqubit_wait() -> None:
     # This test is used for the implementation of the wait instruction
