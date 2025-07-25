abe51b10aea5fb3c22e6c1728a6c78e7b3f7b5b2
Chris Elenbaas <chris.elenbaas@tno.nl>
2025-07-25 17:08:15 +0200

    Update tests.

--- tests/test_parameter_domain.py
+++ tests/test_parameter_domain.py
@@ -6,17 +6,18 @@ import pytest
 
 from opensquirrel import Circuit, CircuitBuilder, X
 from opensquirrel.common import normalize_angle
-from opensquirrel.ir import Float, Gate, Instruction
+from opensquirrel.ir import Gate, Rn, Rx, ControlledGate, I, Rz, Ry
+from opensquirrel.passes.merger import SingleQubitGatesMerger
 
 AnglesType = Iterator[tuple[float, float]]
+
+DELTA = 0.001
 
 
 @pytest.fixture
 def angles() -> AnglesType:
-    delta = 0.1
-    f = 3 / 2
-    input_angles = [pi, -pi, -pi - delta, -pi + delta, f * pi, -f * pi, 2 * pi]
-    expected_outputs = [pi, pi, pi - delta, -pi + delta, -pi / 2, pi / 2, 0.0]
+    input_angles = [pi, -pi, -pi - DELTA, -pi + DELTA, 3 * pi / 2, -3 * pi / 2, 2 * pi]
+    expected_outputs = [pi, pi, pi - DELTA, -pi + DELTA, -pi / 2, pi / 2, 0.0]
     return zip(input_angles, expected_outputs)
 
 
@@ -55,24 +56,33 @@ class TestParsing:
             theta_expected = cast("Float", cast("Instruction", builder.ir.statements[i]).arguments[-1])
             assert theta_expected.value == normalize_angle(2 * pi / 2**k)
 
-        # with pytest.raises(
-        #     TypeError,
-        #     match=r"trying to build 'CRk' with the wrong number or type of arguments: '\(0, 1, 1.5\)': "
-        #           r"parameter 'k' should be of type 'int', but got '<class 'float'>' instead"
-        # ):
-        #     builder.CRk(0, 1, 1.5)
-
     @pytest.mark.parametrize(
-        ("cq_string", "expected_instruction"),
+        ("cq_string", "expected"),
         [
-            # ("version 3; qubit q; inv.X q", [Rn(0, 1.0, 0.0, 0.0, 3.1415927, -1.5707963)]),
+            ("version 3; qubit q; inv.X q", [Rn(0, 1.0, 0.0, 0.0, 3.1415927, -1.5707963)]),
             ("version 3; qubit q; pow(1.0/2).Rn(1.0, 0.0, 0.0, 2 * pi, pi) q", [X(0)]),
-            # ("version 3; qubit q; pow(2).X q", []),
-            # ("version 3; qubit q; pow(4).X q", []),
-            # ("version 3; qubit[2] q; ctrl.Rx(-pi) q[0], q[1]", []),
+            ("version 3; qubit q; pow(2).X q", [Rn(0, 1.0, 0.0, 0.0, 0.0, pi)]),
+            ("version 3; qubit q; pow(4).X q", [Rx(0, 0.0)]),
+            ("version 3; qubit[2] q; ctrl.Rx(-pi) q[0], q[1]", [ControlledGate(0, Rx(1, pi))]),
         ],
-        # ids=["inv", "pow-1/2", "pow-2", "pow-4", "ctrl"]
+        ids=["inv", "pow-1/2", "pow-2", "pow-4", "ctrl"]
     )
-    def test_gate_modifiers(self, cq_string: str, expected_instruction: list[Gate]) -> None:
+    def test_gate_modifiers(self, cq_string: str, expected: list[Gate]) -> None:
         circuit = Circuit.from_string(cq_string)
-        print(circuit)
+        assert circuit.ir.statements == expected
+
+
+class TestMerging:
+    def test_merge_rotation_gates(self) -> None:
+
+        circuit = CircuitBuilder(1).Rx(0, -pi).Rn(0, 1, 0, 0, 0, pi / 2).to_circuit()
+        circuit.merge(merger=SingleQubitGatesMerger())
+        assert circuit.ir.statements[0] == X(0)
+
+        circuit = CircuitBuilder(1).Ry(0, -pi).Ry(0, -DELTA).to_circuit()
+        circuit.merge(merger=SingleQubitGatesMerger())
+        assert circuit.ir.statements[0] == Ry(0, pi - DELTA)
+
+        circuit = CircuitBuilder(1).Rz(0, pi).Rz(0, pi / 2).to_circuit()
+        circuit.merge(merger=SingleQubitGatesMerger())
+        assert circuit.ir.statements[0] == Rz(0, -pi / 2)
