9d5a556d6b483a3d57fb87cae212b672a78cb58c
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-09-26 22:27:44 +0200

    ruff

--- opensquirrel/passes/router/general_router.py
+++ opensquirrel/passes/router/general_router.py
@@ -6,7 +6,7 @@ from opensquirrel.ir import IR
 
 class Router(ABC):
     def __init__(self, **kwargs: Any) -> None:
-        """ Generic router class """
+        """Generic router class"""
 
     @abstractmethod
     def route(self, ir: IR) -> IR:
