65275e79991b17dc03b43e854e675886dd53bdd7
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-09-29 18:22:41 +0200

    Update opensquirrel/circuit.py
    Co-authored-by: Chris Elenbaas <67630508+elenbaasc@users.noreply.github.com>

--- opensquirrel/circuit.py
+++ opensquirrel/circuit.py
@@ -149,6 +149,6 @@ class Circuit:
 
     def count_ops(self) -> dict[str, int]:
         """Count the operations in the circuit by name"""
-        c: Counter[str] = Counter()
-        c.update(getattr(s, "name", "other") for s in self.ir.statements)
+        counter: Counter[str] = Counter()
+        counter.update(getattr(statement, "name", "other") for statement in self.ir.statements)
         return dict(c)
