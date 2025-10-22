122a86f8a5b0fa0d01b6d25f5fe9dc8be26c2b0b
Oancea <rares.oancea@tno.nl>
2025-10-22 07:10:34 +0200

    WIP

--- opensquirrel/passes/router/shortest_path_router.py
+++ opensquirrel/passes/router/shortest_path_router.py
@@ -13,18 +13,9 @@ class ShortestPathRouter(Router):
         super().__init__(**kwargs)
         self.connectivity = connectivity
 
-    def _insert_and_propagate_swaps(self, ir: IR, statement_index: int, shortest_path: list[int]) -> None:
-        for start_qubit_index, end_qubit_index in zip(shortest_path[:-2], shortest_path[1:-1]):
-            ir.statements.insert(statement_index, SWAP(start_qubit_index, end_qubit_index))
-            # Update subsequent statements to reflect the swap
-            for statement in ir.statements[statement_index + 1 :]:
-                if isinstance(statement, Instruction):
-                    for qubit in statement.get_qubit_operands():
-                        if qubit.index == start_qubit_index:
-                            qubit.index = end_qubit_index
-                        elif qubit.index == end_qubit_index:
-                            qubit.index = start_qubit_index
-            statement_index += 1
+    def _get_swaps_for_path(self, shortest_path: list[int]) -> list[tuple[int, int]]:
+        """Generate SWAP pairs for a shortest path."""
+        return [(shortest_path[i], shortest_path[i + 1]) for i in range(len(shortest_path) - 2)]
 
     def route(self, ir: IR) -> IR:
         """
@@ -37,21 +28,87 @@ class ShortestPathRouter(Router):
         """
         graph_data = {int(start_node): end_nodes for start_node, end_nodes in self.connectivity.items()}
         graph = nx.Graph(graph_data)
-        statement_index = 0
-        while statement_index < len(ir.statements):
-            statement = ir.statements[statement_index]
+
+        # Initialize qubit mapping
+        qubit_mapping = {}
+        for statement in ir.statements:
+            if isinstance(statement, Instruction):
+                for qubit in statement.get_qubit_operands():
+                    if qubit.index not in qubit_mapping:
+                        qubit_mapping[qubit.index] = qubit.index
+
+        swaps_to_insert = []
+        temp_mapping = qubit_mapping.copy()
+
+        for statement_idx, statement in enumerate(ir.statements):
             if isinstance(statement, Gate) and len(statement.get_qubit_operands()) == 2:
                 q0, q1 = statement.get_qubit_operands()
-                if not graph.has_edge(q0.index, q1.index):
+                physical_q0 = temp_mapping[q0.index]
+                physical_q1 = temp_mapping[q1.index]
+
+                if not graph.has_edge(physical_q0, physical_q1):
                     try:
-                        shortest_path = nx.shortest_path(graph, source=q0.index, target=q1.index)
-                        # -2 because we skip inserting a swap for the last edge in the path:
-                        # len(path) - 1 edges in total
-                        num_swaps_inserted = len(shortest_path) - 2
-                        self._insert_and_propagate_swaps(ir, statement_index, shortest_path)
-                        statement_index += num_swaps_inserted
+                        shortest_path = nx.shortest_path(graph, source=physical_q0, target=physical_q1)
+                        swaps = self._get_swaps_for_path(shortest_path)
+
+                        for swap_pair in swaps:
+                            insert_position = statement_idx + len(swaps_to_insert)
+                            swaps_to_insert.append((insert_position, SWAP(swap_pair[0], swap_pair[1])))
+
+                            # Update temp mapping to track the effect of this SWAP
+                            logical_at_start = None
+                            logical_at_end = None
+                            for logical, physical in temp_mapping.items():
+                                if physical == swap_pair[0]:
+                                    logical_at_start = logical
+                                elif physical == swap_pair[1]:
+                                    logical_at_end = logical
+
+                            if logical_at_start is not None:
+                                temp_mapping[logical_at_start] = swap_pair[1]
+                            if logical_at_end is not None:
+                                temp_mapping[logical_at_end] = swap_pair[0]
+
                     except nx.NetworkXNoPath as e:
                         msg = f"No routing path available between qubit {q0.index} and qubit {q1.index}"
                         raise NoRoutingPathError(msg) from e
-            statement_index += 1
+
+        new_statements = []
+        swap_index = 0
+        final_mapping = qubit_mapping.copy()
+
+        for _statement_idx, statement in enumerate(ir.statements):
+            while swap_index < len(swaps_to_insert) and swaps_to_insert[swap_index][0] == len(new_statements):
+                swap_gate = swaps_to_insert[swap_index][1]
+                new_statements.append(swap_gate)
+
+                swap_qubits = swap_gate.get_qubit_operands()
+                swap_pair = (swap_qubits[0].index, swap_qubits[1].index)
+
+                logical_at_start = None
+                logical_at_end = None
+                for logical, physical in final_mapping.items():
+                    if physical == swap_pair[0]:
+                        logical_at_start = logical
+                    elif physical == swap_pair[1]:
+                        logical_at_end = logical
+
+                if logical_at_start is not None:
+                    final_mapping[logical_at_start] = swap_pair[1]
+                if logical_at_end is not None:
+                    final_mapping[logical_at_end] = swap_pair[0]
+
+                swap_index += 1
+
+                for qubit in statement.get_qubit_operands():
+                    qubit.index = final_mapping[qubit.index]
+
+            new_statements.append(statement)
+
+        while swap_index < len(swaps_to_insert):
+            new_statements.append(swaps_to_insert[swap_index][1])
+            swap_index += 1
+
+        ir.statements = new_statements
+
         return ir
