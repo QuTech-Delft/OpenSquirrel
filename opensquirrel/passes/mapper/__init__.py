6b7d2b5fe72bd6c152c2942e345938584433af2c
Rares Oancea <rares.oacea@tno.nl>
2025-11-15 15:03:04 +0100

    update __init__.py

--- opensquirrel/passes/mapper/__init__.py
+++ opensquirrel/passes/mapper/__init__.py
@@ -2,6 +2,11 @@ from opensquirrel.passes.mapper.mip_mapper import MIPMapper
 from opensquirrel.passes.mapper.qgym_mapper import QGymMapper
 from opensquirrel.passes.mapper.simple_mappers import HardcodedMapper, IdentityMapper, RandomMapper
 
+try:
+    from opensquirrel.passes.mapper.qgym_mapper import QGymMapper
+except ImportError:
+    QGymMapper = None
+
 __all__ = [
     "HardcodedMapper",
     "IdentityMapper",
