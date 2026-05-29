e9d354131383936c482c04d5e744c2ee8e4d65a1
Chris Elenbaas <chris.elenbaas@tno.nl>
2026-05-29 11:19:31 +0200

    Add 2-qubit gates to standard gate library.

--- opensquirrel/ir/default_gates/two_qubit_gates.py
+++ opensquirrel/ir/default_gates/two_qubit_gates.py
@@ -1,5 +1,5 @@
 import warnings
-from math import pi
+from math import pi, sqrt
 from typing import SupportsFloat, SupportsInt
 
 import numpy as np
@@ -20,27 +20,6 @@ class R(SingleQubitGate):
         super().__init__(qubit=qubit, gate_semantic=BsrAngleParam(axis=(0, 0, 1), angle=theta, phase=phase), name="R")
 
 
-class SWAP(TwoQubitGate):
-    def __init__(self, qubit_0: QubitLike, qubit_1: QubitLike) -> None:
-        super().__init__(
-            qubit0=qubit_0,
-            qubit1=qubit_1,
-            gate_semantic=MatrixGateSemantic(
-                matrix=np.array(
-                    [
-                        [1, 0, 0, 0],
-                        [0, 0, 1, 0],
-                        [0, 1, 0, 0],
-                        [0, 0, 0, 1],
-                    ],
-                ),
-            ),
-            name="SWAP",
-        )
-        self.qubit_0 = Qubit(qubit_0)
-        self.qubit_1 = Qubit(qubit_1)
-
-
 class CNOT(TwoQubitGate):
     def __init__(self, control_qubit: QubitLike, target_qubit: QubitLike) -> None:
         super().__init__(
@@ -48,18 +27,6 @@ class CNOT(TwoQubitGate):
             qubit1=target_qubit,
             gate_semantic=ControlledGateSemantic(BsrNoParams(axis=(1, 0, 0), angle=pi, phase=pi / 2)),
             name="CNOT",
-        )
-        self.control_qubit = Qubit(control_qubit)
-        self.target_qubit = Qubit(target_qubit)
-
-
-class CZ(TwoQubitGate):
-    def __init__(self, control_qubit: QubitLike, target_qubit: QubitLike) -> None:
-        super().__init__(
-            qubit0=control_qubit,
-            qubit1=target_qubit,
-            gate_semantic=ControlledGateSemantic(BsrNoParams(axis=(0, 0, 1), angle=pi, phase=pi / 2)),
-            name="CZ",
         )
         self.control_qubit = Qubit(control_qubit)
         self.target_qubit = Qubit(target_qubit)
@@ -102,3 +69,237 @@ class CRk(TwoQubitGate):
     @property
     def arguments(self) -> tuple[Expression, ...]:
         return (*super().arguments, self.k)
+
+
+class CV(TwoQubitGate):
+    def __init__(self, qubit_0: QubitLike, qubit_1: QubitLike) -> None:
+        super().__init__(
+            qubit0=qubit_0,
+            qubit1=qubit_1,
+            gate_semantic=MatrixGateSemantic(
+                matrix=np.array(
+                    [
+                        [1, 0, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 1 + 1j, 1 - 1j],
+                        [0, 0, 1 - 1j, 1 + 1j],
+                    ],
+                ),
+            ),
+            name="CV",
+        )
+        self.qubit_0 = Qubit(qubit_0)
+        self.qubit_1 = Qubit(qubit_1)
+
+
+class CY(TwoQubitGate):
+    def __init__(self, control_qubit: QubitLike, target_qubit: QubitLike) -> None:
+        super().__init__(
+            qubit0=control_qubit,
+            qubit1=target_qubit,
+            gate_semantic=ControlledGateSemantic(BsrNoParams(axis=(0, 1, 0), angle=pi, phase=pi / 2)),
+            name="CY",
+        )
+        self.control_qubit = Qubit(control_qubit)
+        self.target_qubit = Qubit(target_qubit)
+
+
+class CZ(TwoQubitGate):
+    def __init__(self, control_qubit: QubitLike, target_qubit: QubitLike) -> None:
+        super().__init__(
+            qubit0=control_qubit,
+            qubit1=target_qubit,
+            gate_semantic=ControlledGateSemantic(BsrNoParams(axis=(0, 0, 1), angle=pi, phase=pi / 2)),
+            name="CZ",
+        )
+        self.control_qubit = Qubit(control_qubit)
+        self.target_qubit = Qubit(target_qubit)
+
+
+class DCNOT(TwoQubitGate):
+    def __init__(self, qubit_0: QubitLike, qubit_1: QubitLike) -> None:
+        super().__init__(
+            qubit0=qubit_0,
+            qubit1=qubit_1,
+            gate_semantic=MatrixGateSemantic(
+                matrix=np.array(
+                    [
+                        [1, 0, 0, 0],
+                        [0, 0, 1, 0],
+                        [0, 0, 0, 1],
+                        [0, 1, 0, 0],
+                    ],
+                ),
+            ),
+            name="DCNOT",
+        )
+        self.qubit_0 = Qubit(qubit_0)
+        self.qubit_1 = Qubit(qubit_1)
+
+
+class ECR(TwoQubitGate):
+    def __init__(self, qubit_0: QubitLike, qubit_1: QubitLike) -> None:
+        super().__init__(
+            qubit0=qubit_0,
+            qubit1=qubit_1,
+            gate_semantic=MatrixGateSemantic(
+                matrix=np.array(
+                    [
+                        [0, 0, 1, 1j],
+                        [0, 0, 1j, 1],
+                        [1, -1j, 0, 0],
+                        [-1j, 1, 0, 0],
+                    ],
+                ),
+            ),
+            name="ECR",
+        )
+        self.qubit_0 = Qubit(qubit_0)
+        self.qubit_1 = Qubit(qubit_1)
+
+
+class InvSqrtSWAP(TwoQubitGate):
+    def __init__(self, qubit_0: QubitLike, qubit_1: QubitLike) -> None:
+        super().__init__(
+            qubit0=qubit_0,
+            qubit1=qubit_1,
+            gate_semantic=MatrixGateSemantic(
+                matrix=np.array(
+                    [
+                        [1, 0, 0, 0],
+                        [0, (1j - 1) / 2, (1j + 1) / 2, 0],
+                        [0, (1j + 1) / 2, (1j - 1) / 2, 0],
+                        [0, 0, 0, 1],
+                    ],
+                ),
+            ),
+            name="InvSqrtSWAP",
+        )
+        self.qubit_0 = Qubit(qubit_0)
+        self.qubit_1 = Qubit(qubit_1)
+
+
+class ISWAP(TwoQubitGate):
+    def __init__(self, qubit_0: QubitLike, qubit_1: QubitLike) -> None:
+        super().__init__(
+            qubit0=qubit_0,
+            qubit1=qubit_1,
+            gate_semantic=MatrixGateSemantic(
+                matrix=np.array(
+                    [
+                        [1, 0, 0, 0],
+                        [0, 0, 1j, 0],
+                        [0, 1j, 0, 0],
+                        [0, 0, 0, 1],
+                    ],
+                ),
+            ),
+            name="ISWAP",
+        )
+        self.qubit_0 = Qubit(qubit_0)
+        self.qubit_1 = Qubit(qubit_1)
+
+
+class M(TwoQubitGate):
+    def __init__(self, qubit_0: QubitLike, qubit_1: QubitLike) -> None:
+        super().__init__(
+            qubit0=qubit_0,
+            qubit1=qubit_1,
+            gate_semantic=MatrixGateSemantic(
+                matrix=np.array(
+                    [
+                        [1, 1j, 0, 0],
+                        [0, 0, 1j, 1],
+                        [0, 0, 1j, -1],
+                        [1, -1j, 0, 0],
+                    ],
+                ),
+            ),
+            name="M",
+        )
+        self.qubit_0 = Qubit(qubit_0)
+        self.qubit_1 = Qubit(qubit_1)
+
+
+class MS(TwoQubitGate):
+    def __init__(self, qubit_0: QubitLike, qubit_1: QubitLike) -> None:
+        super().__init__(
+            qubit0=qubit_0,
+            qubit1=qubit_1,
+            gate_semantic=MatrixGateSemantic(
+                matrix=np.array(
+                    [
+                        [1, 0, 0, 1j],
+                        [0, 1, 1j, 0],
+                        [0, 1j, 1, 0],
+                        [1j, 0, 0, 1],
+                    ],
+                ),
+            ),
+            name="MS",
+        )
+        self.qubit_0 = Qubit(qubit_0)
+        self.qubit_1 = Qubit(qubit_1)
+
+
+class SqrtISWAP(TwoQubitGate):
+    def __init__(self, qubit_0: QubitLike, qubit_1: QubitLike) -> None:
+        super().__init__(
+            qubit0=qubit_0,
+            qubit1=qubit_1,
+            gate_semantic=MatrixGateSemantic(
+                matrix=np.array(
+                    [
+                        [1, 0, 0, 0],
+                        [0, 1 / sqrt(2), 1j / sqrt(2), 0],
+                        [0, 1j / sqrt(2), 1 / sqrt(2), 0],
+                        [0, 0, 0, 1],
+                    ],
+                ),
+            ),
+            name="SqrtISWAP",
+        )
+        self.qubit_0 = Qubit(qubit_0)
+        self.qubit_1 = Qubit(qubit_1)
+
+
+class SqrtSWAP(TwoQubitGate):
+    def __init__(self, qubit_0: QubitLike, qubit_1: QubitLike) -> None:
+        super().__init__(
+            qubit0=qubit_0,
+            qubit1=qubit_1,
+            gate_semantic=MatrixGateSemantic(
+                matrix=np.array(
+                    [
+                        [1, 0, 0, 0],
+                        [0, (1j + 1) / 2, (1j - 1) / 2, 0],
+                        [0, (1j - 1) / 2, (1j + 1) / 2, 0],
+                        [0, 0, 0, 1],
+                    ],
+                ),
+            ),
+            name="SqrtSWAP",
+        )
+        self.qubit_0 = Qubit(qubit_0)
+        self.qubit_1 = Qubit(qubit_1)
+
+
+class SWAP(TwoQubitGate):
+    def __init__(self, qubit_0: QubitLike, qubit_1: QubitLike) -> None:
+        super().__init__(
+            qubit0=qubit_0,
+            qubit1=qubit_1,
+            gate_semantic=MatrixGateSemantic(
+                matrix=np.array(
+                    [
+                        [1, 0, 0, 0],
+                        [0, 0, 1, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 0, 1],
+                    ],
+                ),
+            ),
+            name="SWAP",
+        )
+        self.qubit_0 = Qubit(qubit_0)
+        self.qubit_1 = Qubit(qubit_1)
