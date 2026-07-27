be923badd2616694df96e3812e10379131ff8580
David Bakker <david.bakker@tno.nl>
2026-07-27 15:57:32 +0200

    TwoQubitGatesMerger now passes all tests

--- opensquirrel/passes/merger/two_qubit_gates_merger.py
+++ opensquirrel/passes/merger/two_qubit_gates_merger.py
@@ -1,43 +1,95 @@
-from copy import copy
+from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+import networkx as nx
 import numpy as np
 
-from opensquirrel.circuit import Circuit
 from opensquirrel.circuit_builder import CircuitBuilder
 from opensquirrel.circuit_matrix_calculator import get_circuit_matrix
-from opensquirrel.ir import IR, Gate, Qubit
+from opensquirrel.ir import IR, Gate, Instruction, Qubit
 from opensquirrel.ir.semantics.matrix_gate import MatrixGateSemantic
 from opensquirrel.ir.single_qubit_gate import SingleQubitGate
 from opensquirrel.ir.two_qubit_gate import TwoQubitGate
 from opensquirrel.passes.merger.general_merger import Merger
 
+if TYPE_CHECKING:
+    from opensquirrel.circuit import Circuit
 
-def group_gates(gates: list[list[int]]) -> list[tuple[tuple[int, int], list[int]]]:
-    groups = []
-    group = set()
-    statement_indices = []
-    for i, qubit_indices in enumerate(gates):
-        # Reset group if current statement is not a Gate
-        if not qubit_indices:
-            if group:
-                groups.append((tuple(group), copy(statement_indices)))
-            group.clear()
-            statement_indices.clear()
+
+def build_graph(ir: IR) -> nx.DiGraph:
+    n = len(ir.statements)
+    graph = nx.DiGraph()
+    graph.add_nodes_from(
+        (i, {"qubit_indices": statement.qubit_indices})
+        for i, statement in enumerate(ir.statements)
+        if isinstance(statement, Instruction)
+    )
+
+    for i, statement in enumerate(ir.statements):
+        if not isinstance(statement, Instruction):
             continue
 
-        # If the group has more than 2 qubits, it means that we have encountered a new group of gates.
-        if len(group | set(qubit_indices)) > 2:
-            if group:
-                groups.append((tuple(group), copy(statement_indices)))
-            group = set(qubit_indices)
-            statement_indices.clear()
+        qubit_indices = set(statement.qubit_indices)
+        for j in range(i + 1, n):
+            other_statement = ir.statements[j]
+            if isinstance(other_statement, Instruction):
+                other_qubit_indices = set(other_statement.qubit_indices)
 
-        group.update(qubit_indices)
-        statement_indices.append(i)
+                if inter := qubit_indices.intersection(other_qubit_indices):
+                    graph.add_edge(i, j, qubit_index=tuple(inter))
+                    qubit_indices = qubit_indices.difference(inter)
 
-    if group:
-        groups.append((tuple(group), copy(statement_indices)))
-    return groups
+            if not qubit_indices:
+                break
+
+    if not nx.is_directed_acyclic_graph(graph):
+        raise ValueError
+
+    return graph
+
+
+def group_gates(graph: nx.DiGraph) -> list[tuple[set[int], set[int]]]:
+    groups: list[tuple[set, set]] = []
+    available_nodes = set(graph.nodes)
+
+    if len(available_nodes) == 1:
+        return [(available_nodes, set(graph.nodes[0]["qubit_indices"]))]
+
+    for edge in graph.edges():
+        source_node = graph.nodes[edge[0]]
+        target_node = graph.nodes[edge[1]]
+
+        edge_nodes = {edge[0], edge[1]}
+        active_nodes = edge_nodes & available_nodes
+        qubit_indices = set(source_node["qubit_indices"]) | set(target_node["qubit_indices"])
+
+        matched_group = False
+        for group, indices in groups:
+            if not (group & edge_nodes):
+                continue
+
+            if len(indices) == 1:
+                indices.update(qubit_indices)
+
+            if qubit_indices.issubset(indices):
+                group.update(active_nodes)
+                available_nodes.difference_update(active_nodes)
+
+            matched_group = True
+            break
+
+        if not matched_group:
+            groups.append((active_nodes, set(qubit_indices)))
+            available_nodes.difference_update(active_nodes)
+
+    # Sort the groups, such that the order of the two qubit gates is preserved.
+    return sorted(groups, key=lambda x: _first_two_qubit_gate(graph, x[0]))
+
+
+def _first_two_qubit_gate(graph: nx.DiGraph, group: set[int]) -> int:
+    """Return the first statement index pointing to a two qubit gate."""
+    return min(i for i in group if len(graph.nodes[i]["qubit_indices"]) == 2)
 
 
 def normalize_gate_indices(gate: Gate) -> Gate:
@@ -53,11 +105,25 @@ def normalize_gate_indices(gate: Gate) -> Gate:
     msg = f"Unsupported gate type: {type(gate)}"
     raise TypeError(msg)
 
+
+def _merge_gate_group(ir: IR, group: set[int], qubit_indices: set[int]) -> TwoQubitGate:
+    builder = CircuitBuilder(len(qubit_indices))
+    for index in group:
+        statement = ir.statements[index]
+        if isinstance(statement, Gate):
+            statement = normalize_gate_indices(statement)
+            builder.add_instruction(statement)
+
+    sub_circuit_matrix = _get_sub_circuit_matrix(builder.to_circuit())
+    return TwoQubitGate(*qubit_indices, gate_semantic=MatrixGateSemantic(sub_circuit_matrix))
+
+
 def _get_sub_circuit_matrix(circuit: Circuit) -> np.ndarray:
     # `get_circuit_matrix` uses the convention of the first qubit being the most significant bit,
     # so we need to swap the qubits before and after calculating the matrix
     swap = np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
     return swap @ get_circuit_matrix(circuit) @ swap
+
 
 class TwoQubitGatesMerger(Merger):
     def merge(self, ir: IR, qubit_register_size: int) -> None:
@@ -68,38 +134,9 @@ class TwoQubitGatesMerger(Merger):
             qubit_register_size (int): Size of the qubit register
 
         """
+        graph = build_graph(ir)
+        if len(graph.nodes) == 1:
+            return
 
-        gates = [statement.qubit_indices if isinstance(statement, Gate) else [] for statement in ir.statements]
-        groups = group_gates(gates)
-
-        new_gates = []
-
-        for qubit_indices, statement_indices in groups:
-            if len(statement_indices) == 1:
-                new_gates.append(ir.statements[statement_indices[0]])
-                continue
-
-            builder = CircuitBuilder(len(qubit_indices))
-            for statement_index in statement_indices:
-                statement = ir.statements[statement_index]
-                if isinstance(statement, Gate):
-                    statement = normalize_gate_indices(statement)
-                    builder.add_instruction(statement)
-
-            sub_circuit_matrix = _get_sub_circuit_matrix(builder.to_circuit())
-            gate = TwoQubitGate(*qubit_indices, gate_semantic=MatrixGateSemantic(sub_circuit_matrix))
-            new_gates.append(gate)
-
-        # Replace the original statements with the merged gates
-        new_statements = []
-        current_index = 0
-        for new_gate, (_, statement_indices) in zip(new_gates, groups, strict=True):
-            if current_index < statement_indices[0]:
-                new_statements.extend(ir.statements[current_index : statement_indices[0]])
-
-            new_statements.append(new_gate)
-            current_index = statement_indices[-1] + 1
-
-        # Replace the original statements with the merged gates
-        ir.statements = new_statements
-
+        groups = group_gates(graph)
+        ir.statements = [_merge_gate_group(ir, group, qubit_indices) for group, qubit_indices in groups]
--- tests/passes/merger/test_two_qubit_gates_merger.py
+++ tests/passes/merger/test_two_qubit_gates_merger.py
@@ -1,33 +1,76 @@
 import pytest
 
-from opensquirrel import circuit_matrix_calculator
+from opensquirrel import CNOT, H, circuit_matrix_calculator
 from opensquirrel.circuit import Circuit
 from opensquirrel.circuit_builder import CircuitBuilder
 from opensquirrel.common import are_matrices_equivalent_up_to_global_phase
-from opensquirrel.passes.merger.two_qubit_gates_merger import TwoQubitGatesMerger, group_gates
+from opensquirrel.ir import IR, Statement
+from opensquirrel.passes.merger.two_qubit_gates_merger import TwoQubitGatesMerger, build_graph, group_gates
+
+
+class TestNodeGraph:
+    @pytest.mark.parametrize(
+        ("statements", "expected_edges"),
+        [
+            (
+                [H(0), H(1), H(2)],
+                set(),
+            ),
+            (
+                [H(0), CNOT(0, 1), H(1)],
+                {(0, 1), (1, 2)},
+            ),
+            (
+                [H(1), CNOT(0, 1), H(1), H(2), CNOT(1, 2), H(2)],
+                {(0, 1), (1, 2), (2, 4), (3, 4), (4, 5)},
+            ),
+            (
+                [H(3), CNOT(2, 3), H(1), H(2), CNOT(0, 1), H(2), H(1), H(3), CNOT(1, 2), H(2)],
+                {(0, 1), (1, 3), (1, 7), (6, 8), (2, 4), (4, 6), (3, 5), (5, 8), (8, 9)},
+            ),
+        ],
+    )
+    def test_build_graph_creates_dependencies(
+        self, statements: list[Statement], expected_edges: set[tuple[int, int]]
+    ) -> None:
+        ir = IR()
+        for statement in statements:
+            ir.add_statement(statement)
+
+        graph = build_graph(ir)
+
+        assert set(graph.edges()) == expected_edges
+
+    @pytest.mark.parametrize(
+        ("statements", "expected"),
+        [
+            ([H(0)], [({0}, {0})]),
+            ([CNOT(0, 1)], [({0}, {0, 1})]),
+            ([H(0), CNOT(0, 1), H(1)], [({0, 1, 2}, {0, 1})]),
+            ([H(1), CNOT(0, 1), H(1), H(2), CNOT(1, 2), H(2)], [({0, 1, 2}, {0, 1}), ({3, 4, 5}, {1, 2})]),
+            (
+                [H(3), CNOT(2, 3), H(1), H(2), CNOT(0, 1), H(2), H(1), H(3), CNOT(1, 2), H(2)],
+                [({0, 1, 3, 5, 7}, {2, 3}), ({2, 4, 6}, {0, 1}), ({8, 9}, {1, 2})],
+            ),
+            (
+                [H(2), H(0), CNOT(0, 1), CNOT(1, 2), CNOT(2, 3), H(1), H(3), H(0), H(2)],
+                [({1, 2, 7}, {0, 1}), ({0, 3, 5}, {1, 2}), ({4, 6, 8}, {2, 3})],
+            ),
+        ],
+    )
+    def test_group_gates(self, statements: list[Statement], expected: list[tuple[set[int], set[int]]]) -> None:
+        ir = IR()
+        for statement in statements:
+            ir.add_statement(statement)
+
+        graph = build_graph(ir)
+
+        assert group_gates(graph) == expected
 
 
 @pytest.fixture
 def merger() -> TwoQubitGatesMerger:
     return TwoQubitGatesMerger()
-
-
-@pytest.mark.parametrize(
-    ("gates", "expected_groups"),
-    [
-        ([[0]], [((0,), [0])]),
-        ([[0, 1]], [((0, 1), [0])]),
-        ([[0, 1], [0, 1], [1, 2], [1, 2]], [((0, 1), [0, 1]), ((1, 2), [2, 3])]),
-        ([[0, 1], [], [1, 2], [1, 2]], [((0, 1), [0]), ((1, 2), [2, 3])]),
-        ([[0, 1], [0, 1], [], [1, 2], [1, 2]], [((0, 1), [0, 1]), ((1, 2), [3, 4])]),
-        (
-            [[0, 1], [2, 1], [1, 2], [1], [1], [0, 1], [0], [1]],
-            [((0, 1), [0]), ((1, 2), [1, 2, 3, 4]), ((0, 1), [5, 6, 7])],
-        ),
-    ],
-)
-def test_group_gates(gates: list[list[int]], expected_groups: list[tuple[tuple[int, int], list[int]]]) -> None:
-    assert group_gates(gates) == expected_groups
 
 
 @pytest.mark.parametrize(
@@ -42,8 +85,8 @@ def test_group_gates(gates: list[list[int]], expected_groups: list[tuple[tuple[i
         (CircuitBuilder(2).CV(0, 1).CV(0, 1).to_circuit(), CircuitBuilder(2).CNOT(0, 1).to_circuit()),
         (CircuitBuilder(2).H(0).H(1).CNOT(0, 1).H(0).H(1).to_circuit(), CircuitBuilder(2).CNOT(1, 0).to_circuit()),
         (
-            CircuitBuilder(2).CV(0, 1).to_circuit(),
             CircuitBuilder(2).T(0).H(1).CNOT(1, 0).Tdag(0).T(1).CNOT(1, 0).H(1).to_circuit(),
+            CircuitBuilder(2).CV(0, 1).to_circuit(),
         ),
     ],
 )
